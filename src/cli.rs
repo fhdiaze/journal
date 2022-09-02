2a8a31f2484d08c901edf5698fd7bd2a160f3996
fredyd <fredyd@keoworld.com>
2022-09-02 07:31:45 -0500

    feat: done and list

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,6 @@
 use std::{
-    fs::OpenOptions,
-    io::{Result, Seek, SeekFrom},
+    fs::{File, OpenOptions},
+    io::{Error, ErrorKind, Result, Seek, SeekFrom},
     path::PathBuf,
 };
 use structopt::StructOpt;
@@ -44,15 +44,62 @@ pub fn add_task(journal_path: PathBuf, task: Task) -> Result<()> {
         .write(true)
         .create(true)
         .open(journal_path)?;
-    let mut tasks = match serde_json::from_reader(&file) {
-        Ok(tasks) => tasks,
-        Err(e) if e.is_eof() => Vec::new(),
-        Err(e) => Err(e)?,
-    };
+    let mut tasks = collect_tasks(&file)?;
 
     file.seek(SeekFrom::Start(0))?;
     tasks.push(task);
     serde_json::to_writer(file, &tasks)?;
 
     Ok(())
+}
+
+/// Completes the task within the list which is in the specified position.
+/// 0 < task_position <= list length
+pub fn complete_task(journal_path: PathBuf, task_position: usize) -> Result<()> {
+    let mut file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .open(journal_path)?;
+    let mut tasks: Vec<Task> = collect_tasks(&file)?;
+
+    if task_position > tasks.len() {
+        return Err(Error::new(ErrorKind::InvalidInput, "Invalid task id"));
+    }
+    tasks.remove(task_position - 1);
+
+    file.seek(SeekFrom::Start(0))?;
+    file.set_len(0)?;
+
+    serde_json::to_writer(file, &tasks)?;
+    Ok(())
+}
+
+/// Lists the tasks
+pub fn list_tasks(journal_path: PathBuf) -> Result<()> {
+    let file = OpenOptions::new().read(true).open(journal_path)?;
+    let tasks = collect_tasks(&file)?;
+
+    if tasks.is_empty() {
+        println!("Task list is empty!");
+    } else {
+        let mut order: u32 = 1;
+        for t in tasks {
+            println!("{}: {}", order, t);
+        }
+    }
+
+    Ok(())
+}
+
+/// Reads tasks from the specified file
+fn collect_tasks(mut file: &File) -> Result<Vec<Task>> {
+    file.seek(SeekFrom::Start(0))?;
+    let tasks = match serde_json::from_reader(file) {
+        Ok(tasks) => tasks,
+        Err(e) if e.is_eof() => Vec::new(),
+        Err(e) => Err(e)?,
+    };
+    file.seek(SeekFrom::Start(0))?;
+
+    Ok(tasks)
 }
--- src/task.rs
+++ src/task.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 use chrono::{serde::ts_seconds, DateTime, Local, Utc};
 use serde::{Deserialize, Serialize};
 
@@ -12,5 +14,12 @@ impl Task {
     pub fn new(text: String) -> Self {
         let created_at = Utc::now();
         Task { text, created_at }
+    }
+}
+
+impl Display for Task {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let created_at = self.created_at.with_timezone(&Local).format("%F %H:%M");
+        write!(f, "{:<50} [{}]", self.text, created_at)
     }
 }
